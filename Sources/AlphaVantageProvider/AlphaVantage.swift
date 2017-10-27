9b32745b3a29381bce0029cb44392d96af526bd1
AJ Bartocci <aministellae@yahoo.com>
2017-10-26 23:19:18 -0400

    uncommenting

--- Sources/AlphaVantageProvider/AlphaVantage.swift
+++ Sources/AlphaVantageProvider/AlphaVantage.swift
@@ -1,99 +1,100 @@
-////
-////  AlphaVantage.swift
-////  AlphaVantagePackageDescription
-////
-////  Created by AJ Bartocci on 10/22/17.
-////
 //
-//import Foundation
-//import AlphaVantage
+//  AlphaVantage.swift
+//  AlphaVantagePackageDescription
 //
-//final public class AlphaVantageService {
-//    public static func setup(apiKey: String, session: URLSession = .shared) {
-//        AlphaVantage.shared.apiKey = apiKey
-//        AlphaVantage.shared.session = session
-//    }
-//}
+//  Created by AJ Bartocci on 10/22/17.
 //
-//// expose the session as an optional which allows us to inject a custom session for testing
-//// maybe expose more like the session to make it testable?
-////
-////*/
+
+import Foundation
+import AlphaVantage
+
+final public class AlphaVantageService {
+    public static func setup(apiKey: String, session: URLSession = .shared) {
+        AlphaVantage.shared.apiKey = apiKey
+        AlphaVantage.shared.session = session
+    }
+}
+
+// expose the session as an optional which allows us to inject a custom session for testing
+// maybe expose more like the session to make it testable?
 //
-//
-//public struct AlphaVantage {
-//    fileprivate let apiErrorMessage = "API key has not been set. You must call AlphaVantage.setup(with apiKey) before attempting to make any calls. Typically set during app launch"
-//    fileprivate var apiKey: String?
-//    fileprivate var session: URLSession?
-//    public static var shared = AlphaVantage() 
-//    
-//    fileprivate init() { }
-//    init(apiKey: String, session: URLSession) {
-//        self.apiKey = apiKey
-//        self.session = session
-//    }
-//    
-//    public lazy var timeSeries: TimeSeriesWrapper = {
-//        var series = TimeSeriesWrapper(errorMessage: self.apiErrorMessage)
-//        series.apiKey = self.apiKey
-//        return series
-//    }()
-//}
-//
-//public struct TimeSeriesWrapper {
-//    fileprivate var apiKey: String?
-//    private let apiErrorMessage: String
-//    fileprivate init(errorMessage: String) {
-//        self.apiErrorMessage = errorMessage
-//    }
-//    /// Provides calls to get JSON or IntraDayModel responses
-//    public lazy var intraDay: AVFIntraDayWorker = {
-//        guard let key = self.apiKey else {
-//            fatalError(self.apiErrorMessage)
-//        }
-//        return AVFIntraDayWorker(apiKey: key)
-//    }()
-//    /// Provides calls to get JSON or DailyModel responses
-//    public lazy var daily: AVFNonIntervalWorker<AVFDailyConfig> = {
-//        guard let key = self.apiKey else {
-//            fatalError(self.apiErrorMessage)
-//        }
-//        return AVFNonIntervalWorker<AVFDailyConfig>(apiKey: key)
-//    }()
-//    /// Provides calls to get JSON or DailyAdjustedModel responses
-//    public lazy var dailyAdjusted: AVFNonIntervalWorker<AVFDailyAdjustedConfig> = {
-//        guard let key = self.apiKey else {
-//            fatalError(self.apiErrorMessage)
-//        }
-//        return AVFNonIntervalWorker<AVFDailyAdjustedConfig>(apiKey: key)
-//    }()
-//    /// Provides calls to get JSON or WeeklyModel responses
-//    public lazy var weekly: AVFNonIntervalWorker<AVFWeeklyConfig> = {
-//        guard let key = self.apiKey else {
-//            fatalError(self.apiErrorMessage)
-//        }
-//        return AVFNonIntervalWorker<AVFWeeklyConfig>(apiKey: key)
-//    }()
-//    /// Provides calls to get JSON or WeeklyAdjustedModel responses
-//    public lazy var weeklyAdjusted: AVFNonIntervalWorker<AVFWeeklyAdjustedConfig> = {
-//        guard let key = self.apiKey else {
-//            fatalError(self.apiErrorMessage)
-//        }
-//        return AVFNonIntervalWorker<AVFWeeklyAdjustedConfig>(apiKey: key)
-//    }()
-//    /// Provides calls to get JSON or MonthlyModel responses
-//    public lazy var monthly: AVFNonIntervalWorker<AVFMonthlyConfig> = {
-//        guard let key = self.apiKey else {
-//            fatalError(self.apiErrorMessage)
-//        }
-//        return AVFNonIntervalWorker<AVFMonthlyConfig>(apiKey: key)
-//    }()
-//    /// Provides calls to get JSON or MonthlyAdjustedModel responses
-//    public lazy var monthlyAdjusted: AVFNonIntervalWorker<AVFMonthlyAdjustedConfig> = {
-//        guard let key = self.apiKey else {
-//            fatalError(self.apiErrorMessage)
-//        }
-//        return AVFNonIntervalWorker<AVFMonthlyAdjustedConfig>(apiKey: key)
-//    }()
-//}
-//
+//*/
+
+
+public struct AlphaVantage {
+    fileprivate let apiErrorMessage = "API key has not been set. You must call AlphaVantage.setup(with apiKey) before attempting to make any calls. Typically set during app launch"
+    fileprivate var apiKey: String?
+    fileprivate var session: URLSession?
+    public static var shared = AlphaVantage() 
+    
+    fileprivate init() { }
+    init(apiKey: String, session: URLSession) {
+        self.apiKey = apiKey
+        self.session = session
+    }
+    
+    public lazy var timeSeries: TimeSeriesWrapper = {
+        var series = TimeSeriesWrapper(errorMessage: self.apiErrorMessage)
+        series.apiKey = self.apiKey
+        return series
+    }()
+}
+
+public struct TimeSeriesWrapper {
+    fileprivate var apiKey: String?
+    private let apiErrorMessage: String
+    fileprivate init(errorMessage: String) {
+        self.apiErrorMessage = errorMessage
+    }
+    /// Provides calls to get JSON or IntraDayModel responses
+    public lazy var intraDay: AVFIntraDayWorker = {
+        guard let key = self.apiKey else {
+            fatalError(self.apiErrorMessage)
+        }
+        return AVFIntraDayWorker(apiKey: key)
+    }()
+    /// Provides calls to get JSON or DailyModel responses
+    public lazy var daily: AVFNonIntervalWorker<AVFDailyConfig> = {
+        guard let key = self.apiKey else {
+            fatalError(self.apiErrorMessage)
+        }
+        return AVFNonIntervalWorker<AVFDailyConfig>(apiKey: key)
+    }()
+    /// Provides calls to get JSON or DailyAdjustedModel responses
+    public lazy var dailyAdjusted: AVFNonIntervalWorker<AVFDailyAdjustedConfig> = {
+        guard let key = self.apiKey else {
+            fatalError(self.apiErrorMessage)
+        }
+        return AVFNonIntervalWorker<AVFDailyAdjustedConfig>(apiKey: key)
+    }()
+    /// Provides calls to get JSON or WeeklyModel responses
+    public lazy var weekly: AVFNonIntervalWorker<AVFWeeklyConfig> = {
+        guard let key = self.apiKey else {
+            fatalError(self.apiErrorMessage)
+        }
+        return AVFNonIntervalWorker<AVFWeeklyConfig>(apiKey: key)
+    }()
+    /// Provides calls to get JSON or WeeklyAdjustedModel responses
+    public lazy var weeklyAdjusted: AVFNonIntervalWorker<AVFWeeklyAdjustedConfig> = {
+        guard let key = self.apiKey else {
+            fatalError(self.apiErrorMessage)
+        }
+        return AVFNonIntervalWorker<AVFWeeklyAdjustedConfig>(apiKey: key)
+    }()
+    /// Provides calls to get JSON or MonthlyModel responses
+    public lazy var monthly: AVFNonIntervalWorker<AVFMonthlyConfig> = {
+        guard let key = self.apiKey else {
+            fatalError(self.apiErrorMessage)
+        }
+        return AVFNonIntervalWorker<AVFMonthlyConfig>(apiKey: key)
+    }()
+    /// Provides calls to get JSON or MonthlyAdjustedModel responses
+    public lazy var monthlyAdjusted: AVFNonIntervalWorker<AVFMonthlyAdjustedConfig> = {
+        guard let key = self.apiKey else {
+            fatalError(self.apiErrorMessage)
+        }
+        return AVFNonIntervalWorker<AVFMonthlyAdjustedConfig>(apiKey: key)
+    }()
+}
+
+
